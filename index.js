6861fb8e577eb795c25388b86f00a317156ce4b4
Paulo Salvatore <salvatoregames@gmail.com>
2022-01-06 12:11:45 -0300

    chore(api): cleanup code and add `.prettierrc.json` file

--- index.js
+++ index.js
@@ -1,46 +1,46 @@
 // DotEnv
-require("dotenv").config();
+require('dotenv').config();
 
 // Auth
-const jwt = require("jsonwebtoken");
-const crypto = require("crypto");
-const fs = require("fs");
+const jwt = require('jsonwebtoken');
+const crypto = require('crypto');
+const fs = require('fs');
 
 // Axios
-const axios = require("axios");
+const axios = require('axios');
 
 // Express
-const express = require("express");
+const express = require('express');
 
 // Auth
 
-const privateKey = fs.readFileSync("./pem/private-key.pem");
+const privateKey = fs.readFileSync('./pem/private-key.pem');
 
 const pubKeyObject = crypto.createPublicKey({
-  key: privateKey,
-  format: "pem",
+    key: privateKey,
+    format: 'pem',
 });
 
 const publicKey = pubKeyObject.export({
-  format: "pem",
-  type: "spki",
+    format: 'pem',
+    type: 'spki',
 });
 
-const appId = "162974";
-const clientId = "Iv1.e643fd1328e57814";
+const appId = '162974';
+const clientId = 'Iv1.e643fd1328e57814';
 
 const issuer = appId;
-const subject = "salvatoregames@gmail.com";
-const audience = "https://poc-github-api.herokuapp.com/";
-const expiresIn = "10m";
-const algorithm = "RS256";
+const subject = 'salvatoregames@gmail.com';
+const audience = 'https://poc-github-api.herokuapp.com/';
+const expiresIn = '10m';
+const algorithm = 'RS256';
 
 const signOptions = {
-  issuer,
-  subject,
-  audience,
-  expiresIn,
-  algorithm,
+    issuer,
+    subject,
+    audience,
+    expiresIn,
+    algorithm,
 };
 
 const payload = {};
@@ -50,11 +50,11 @@ const tokenFromPem = jwt.sign(payload, privateKey, signOptions);
 console.log({ tokenFromPem });
 
 const verifyOptions = {
-  issuer,
-  subject,
-  audience,
-  expiresIn,
-  algorithm,
+    issuer,
+    subject,
+    audience,
+    expiresIn,
+    algorithm,
 };
 
 const legit = jwt.verify(tokenFromPem, publicKey, verifyOptions);
@@ -66,44 +66,44 @@ const githubApiToken = process.env.GITHUB_API_TOKEN;
 // GitHub API
 
 axios
-  .get("https://api.github.com/app", {
-    headers: {
-      Authorization: `Bearer ${tokenFromPem}`,
-      Accept: "application/vnd.github.v3+json",
-    },
-  })
-  .then((response) => {
-    console.log(response.data);
-  })
-  .catch((error) => {
-    console.error(error.message);
-  });
+    .get('https://api.github.com/app', {
+        headers: {
+            Authorization: `Bearer ${tokenFromPem}`,
+            Accept: 'application/vnd.github.v3+json',
+        },
+    })
+    .then(response => {
+        console.log(response.data);
+    })
+    .catch(error => {
+        console.error(error.message);
+    });
 
 // Express App
 
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("Hello World");
+app.get('/', (req, res) => {
+    res.send('Hello World');
 });
 
-app.get("/login", (req, res) => {
-  const url = new URL(`https://github.com/login/oauth/authorize`);
+app.get('/login', (req, res) => {
+    const url = new URL(`https://github.com/login/oauth/authorize`);
 
-  url.searchParams.append("client_id", clientId);
-  url.searchParams.append(
-    "redirect_uri",
-    "https://poc-github-api.herokuapp.com/callback-url"
-  );
-  url.searchParams.append("state", "random_string");
-  url.searchParams.append("login", "dikiwov360");
-  // url.searchParams.append("login", "salvatoregames@gmail.com");
+    url.searchParams.append('client_id', clientId);
+    url.searchParams.append(
+        'redirect_uri',
+        'https://poc-github-api.herokuapp.com/callback-url',
+    );
+    url.searchParams.append('state', 'random_string');
+    url.searchParams.append('login', 'dikiwov360');
+    // url.searchParams.append("login", "salvatoregames@gmail.com");
 
-  res.send(`<a href="${url}">Login</a>`);
+    res.send(`<a href="${url}">Login</a>`);
 });
 
-app.get("/callback-url", (req, res) => {
-  console.log("Callback URL");
+app.get('/callback-url', (req, res) => {
+    console.log('Callback URL');
 });
 
 app.listen(process.env.PORT || 3000);
