80510834f4553ff64e70d7c7acf0e84a4f455089
Paulo Salvatore <salvatoregames@gmail.com>
2022-01-06 11:14:55 -0300

    chore: cleanup code

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
-const express = require('express');
 require("dotenv").config();
 
+const githubApiToken = process.env.GITHUB_API_TOKEN;
+
+const express = require("express");
 const app = express();
- 
-app.get('/', (req, res) => {
-  res.send('Hello World');
+
+app.get("/", (req, res) => {
+    res.send("Hello World");
 });
- 
+
 app.listen(process.env.PORT || 3000);
